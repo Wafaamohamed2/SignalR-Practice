4e971d7bda51738a3600139208847be8e794fedc
Wafaa Mohamed <121331529+Wafaamohamed2@users.noreply.github.com>
2025-09-18 19:47:49 +0300

    first commit

---
+++ SognalR/HubConfig/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SognalR.HubConfig
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+            // dbsave
+
+            await Clients.All.SendAsync("ReceiveMessage", name, message);
+        }
+    }
+}
---
+++ SognalR/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using SognalR.HubConfig;
+
+namespace SognalR
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options => { 
+                options.AddPolicy("AllowAllHeaders", builder => 
+                {
+                  builder.AllowAnyOrigin()
+                         .AllowAnyHeader()
+                         .AllowAnyMethod();
+                });
+            
+            });
+
+            services.AddSignalR(option =>
+            {
+                option.EnableDetailedErrors = true;
+            });
+
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+          if (env.IsDevelopment())
+          {
+              app.UseDeveloperExceptionPage();
+          }
+
+          app.UseRouting();
+          app.UseCors("AllowAllHeaders"); // Apply the CORS policy
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); // for API controllers
+                endpoints.MapHub<ChatHub>("/chatHub"); // Map the ChatHub to the /chatHub endpoint
+            });
+
+        }
+    }
+}
